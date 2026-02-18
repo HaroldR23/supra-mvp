df790bda17644ad4dd28b52109cf2c7593f75464
HaroldR23 <ahsro99@gmail.com>
2026-02-18 16:52:44 -0300

    deployment

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,7 @@
-from pydantic_settings import BaseSettings
-from functools import lru_cache
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
-database_url: str = os.environ.get("DATABASE_URL", "sqlite:///./test.db")
-secret_key: str = os.environ.get("SECRET_KEY", "your-secret-key-for-jwt")
-environment: str = os.environ.get("ENVIRONMENT", "development")
+database_url: str = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+secret_key: str = os.getenv("SECRET_KEY", "your_secret_key")
+environment: str = os.getenv("ENVIRONMENT", "production")
 algorithm: str = "HS256"
 access_token_expire_minutes: int = 30
