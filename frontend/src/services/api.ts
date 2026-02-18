35d51fb2bc152ea431031ab9629aa1d5a97b3340
HaroldR23 <ahsro99@gmail.com>
2026-02-18 13:36:48 -0300

    debug

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+console.log("Environment Variables:", process.env);
 console.log("API URL:", apiUrl);
 export const api = axios.create({
   baseURL: `${apiUrl}api/v1`,
